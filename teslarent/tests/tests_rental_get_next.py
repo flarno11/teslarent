0cc35ffdc805c62c0e835e0cf6456430927e9329
flarno11 <flarno11@gmail.com>
2019-08-17 12:02:48 +0200

    backport update unit tests

--- teslarent/tests/tests_rental_get_next.py
+++ teslarent/tests/tests_rental_get_next.py
@@ -3,7 +3,7 @@ import datetime
 from django.test import TestCase
 
 from teslarent.models import *
-from teslarent.tests.tests_rental_update_start_end import RentalStartEndTestCase
+from teslarent.tests.tests_teslaapi import TeslaApiTestCase
 
 
 class RentalGetNextCase(TestCase):
@@ -11,37 +11,37 @@ class RentalGetNextCase(TestCase):
         pass
 
     def test_start_end_in_future(self):
-        RentalStartEndTestCase.create_credentials()
-        RentalStartEndTestCase.create_vehicle()
+        TeslaApiTestCase.create_credentials()
+        TeslaApiTestCase.create_vehicle_default()
 
         now = timezone.now()
         start = now + datetime.timedelta(days=1)
         end = now + datetime.timedelta(days=2)
-        RentalStartEndTestCase.create_rental(start=start, end=end)
+        TeslaApiTestCase.create_rental(start=start, end=end)
 
         next_rental_at = Rental.get_next_rental_start_or_end_time(now)
         self.assertEqual(start, next_rental_at)
 
     def test_start_in_past_end_in_future(self):
-        RentalStartEndTestCase.create_credentials()
-        RentalStartEndTestCase.create_vehicle()
+        TeslaApiTestCase.create_credentials()
+        TeslaApiTestCase.create_vehicle_default()
 
         now = timezone.now()
         start = now - datetime.timedelta(days=1)
         end = now + datetime.timedelta(days=1)
-        RentalStartEndTestCase.create_rental(start=start, end=end)
+        TeslaApiTestCase.create_rental(start=start, end=end)
 
         next_rental_at = Rental.get_next_rental_start_or_end_time(now)
         self.assertEqual(end, next_rental_at)
 
     def test_start_end_in_past(self):
-        RentalStartEndTestCase.create_credentials()
-        RentalStartEndTestCase.create_vehicle()
+        TeslaApiTestCase.create_credentials()
+        TeslaApiTestCase.create_vehicle_default()
 
         now = timezone.now()
         start = now - datetime.timedelta(days=2)
         end = now - datetime.timedelta(days=1)
-        RentalStartEndTestCase.create_rental(start=start, end=end)
+        TeslaApiTestCase.create_rental(start=start, end=end)
 
         next_rental_at = Rental.get_next_rental_start_or_end_time(now)
         self.assertIsNone(next_rental_at)
--- teslarent/tests/tests_rental_update_start_end.py
+++ teslarent/tests/tests_rental_update_start_end.py
@@ -7,47 +7,20 @@ import requests_mock
 
 from teslarent.management.commands import rental_start_end
 from teslarent.models import *
-from teslarent.tests.tests_teslaapi import CURRENT_VEHICLE_ID, VEHICLE_DATA_RESPONSE
+from teslarent.tests.tests_teslaapi import CURRENT_VEHICLE_ID, VEHICLE_DATA_RESPONSE, TeslaApiTestCase
 
 
 class RentalStartEndTestCase(TestCase):
-
     def setUp(self):
         pass
 
-    @staticmethod
-    def create_credentials():
-        c = Credentials(email="test@test.com")
-        c.update_token("1234", "5678", 10*24*3600)
-        c.save()
-        return c
-
-    @staticmethod
-    def create_vehicle():
-        v = Vehicle()
-        v.id = CURRENT_VEHICLE_ID
-        v.vehicle_id = CURRENT_VEHICLE_ID * 10
-        v.display_name = ""
-        v.credentials = Credentials.objects.all()[0]
-        v.linked = True
-        v.mobile_enabled = True
-        v.save()
-        return v
-
-    @staticmethod
-    def create_rental(start, end):
-        r = Rental(start=start, end=end, code=(uuid.uuid4()))
-        r.vehicle = Vehicle.objects.get(id=CURRENT_VEHICLE_ID)
-        r.save()
-        return r
-
     def test_update_start(self):
-        self.create_credentials()
-        self.create_vehicle()
+        TeslaApiTestCase.create_credentials()
+        TeslaApiTestCase.create_vehicle_default()
 
         start = timezone.now()
         end = timezone.now() + datetime.timedelta(days=1)
-        self.create_rental(start=start, end=end)
+        TeslaApiTestCase.create_rental(start=start, end=end)
 
         r = Rental.objects.all()[0]
         self.assertIsNone(r.odometer_start)
