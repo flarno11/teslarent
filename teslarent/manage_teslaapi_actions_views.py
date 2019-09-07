0df53f4efa7f9b69b21aac1b47f5cd31deca6ebd
flarno11 <flarno11@gmail.com>
2019-09-07 18:22:09 +0200

    bugfix missing parameter

--- teslarent/manage_teslaapi_actions_views.py
+++ teslarent/manage_teslaapi_actions_views.py
@@ -18,7 +18,7 @@ def refresh_credentials(request, credentials_id):
         c = Credentials.objects.get(id=credentials_id)
         teslaapi.refresh_token(c)
 
-    return redirect('/manage/')
+    return redirect('manage:index')
 
 
 @staff_member_required
@@ -27,8 +27,8 @@ def update_vehicles(request):
         raise Http404
 
     log.warning('update_vehicles')
-    teslaapi.update_all_vehicles()
-    return redirect('/manage/')
+    teslaapi.update_all_vehicles(wake_up_vehicle=True)
+    return redirect('manage:index')
 
 
 @staff_member_required
@@ -41,7 +41,7 @@ def lock_vehicle(request, vehicle_id):
     ensure_vehicle_is_awake(vehicle)
     teslaapi.lock_vehicle(vehicle)
     fetch_and_save_vehicle_state(vehicle)
-    return redirect('/manage/')
+    return redirect('manage:index')
 
 
 @staff_member_required
@@ -54,4 +54,40 @@ def unlock_vehicle(request, vehicle_id):
     ensure_vehicle_is_awake(vehicle)
     teslaapi.unlock_vehicle(vehicle)
     fetch_and_save_vehicle_state(vehicle)
-    return redirect('/manage/')
+    return redirect('manage:index')
+
+
+def valet_mode_enable(request, vehicle_id):
+    if request.method != "POST":
+        raise Http404
+
+    log.warning('valet_mode_enable vehicle=%s' % (str(vehicle_id)))
+    vehicle = Vehicle.objects.get(id=vehicle_id)
+    ensure_vehicle_is_awake(vehicle)
+    teslaapi.enable_valet_mode(vehicle, 3740)
+    fetch_and_save_vehicle_state(vehicle)
+    return redirect('manage:vehicle-list')
+
+
+def valet_mode_disable(request, vehicle_id):
+    if request.method != "POST":
+        raise Http404
+
+    log.warning('valet_mode_disable vehicle=%s' % (str(vehicle_id)))
+    vehicle = Vehicle.objects.get(id=vehicle_id)
+    ensure_vehicle_is_awake(vehicle)
+    teslaapi.disable_valet_mode(vehicle)
+    fetch_and_save_vehicle_state(vehicle)
+    return redirect('manage:vehicle-list')
+
+
+def speed_limit_mode_disable(request, vehicle_id):
+    if request.method != "POST":
+        raise Http404
+
+    log.warning('speed_limit_mode_disable vehicle=%s' % (str(vehicle_id)))
+    vehicle = Vehicle.objects.get(id=vehicle_id)
+    ensure_vehicle_is_awake(vehicle)
+    teslaapi.disable_speed_limit(vehicle)
+    fetch_and_save_vehicle_state(vehicle)
+    return redirect('manage:vehicle-list')
