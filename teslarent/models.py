84620c598238b111b20ef7c713010776b01d1580
flarno11 <flarno11@gmail.com>
2022-04-01 15:01:45 +0200

    increase token size

--- teslarent/models.py
+++ teslarent/models.py
@@ -17,8 +17,8 @@ class Credentials(models.Model):
     email = models.EmailField(max_length=200, unique=True)
     salt = models.CharField(max_length=32, verbose_name="Salt for KDF from secret to encryption key")
     iv = models.CharField(max_length=32, verbose_name="Initialization Vector for token encryption")
-    current_token = models.CharField(max_length=512)  # currently the tokens seem to be 160 hex bytes (encrypted) long
-    refresh_token = models.CharField(max_length=2048)  # currently the tokens seem to be 1800 hex bytes (encrypted) long
+    current_token = models.CharField(max_length=3072)  # currently the tokens seem to be 160 hex bytes (encrypted) long
+    refresh_token = models.CharField(max_length=3072)  # currently the tokens seem to be 1800 hex bytes (encrypted) long
     token_expires_at = models.DateTimeField()
 
     class Meta:
